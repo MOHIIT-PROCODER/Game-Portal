1f9196d8ebba9782e6b4592cd28f9dbc8a698631
MOHIT-650 <mohantasarojkumar65@gmail.com>
2026-07-25 20:38:06 +0530

    fix: reset scroll position to top on route change

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import { GameProvider } from "./context/GameContext";
 import AppRoutes from "./routes/AppRoutes";
+import ScrollToTop from "./components/common/ScrollToTop";
 
 export function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <GameProvider>
         <AppRoutes />
       </GameProvider>
